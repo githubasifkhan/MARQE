3809fe367abafcf8ea7baafaa2eaba66c39d8561
Nadia Faqir Muhammad <nadiafaqirm@gmail.com>
2022-10-28 15:16:41 -0700

    Update fields.py

--- ol_property_custom/models/fields.py
+++ ol_property_custom/models/fields.py
@@ -1,4 +1,5 @@
 from datetime import date
+import this
 from odoo import api, fields, models, _
 from openerp.tools.translate import _
 from openerp.exceptions import UserError
@@ -11,6 +12,16 @@ class Sales_Data(models.Model):
     project=fields.Many2one(comodel_name='project.project', string='Project')
     floor=fields.Many2one(comodel_name='property.floor', string='Floor')
     unit=fields.Many2one(comodel_name='product.product', string='Unit')
+
+    @api.onchange('unit')
+    def onchange_unit(self):
+        rec =self.env['product.product'].search([('id','=',self.unit.id)])
+        if rec:
+             self.building = rec.building
+             self.floor=rec.floor_id
+             self.project=rec.project
+
+
 
 class inv_Data(models.Model):
 
